f47087f3a4abf053d9f1e1fe8a2f619a7cb22d1d
Rakesh <rakesh@rakesh-Vostro-1550.(none)>
2014-07-05 11:43:43 +0530

    post crad file (img) upload problem solved ie. suhosin

--- lib/View/Server/PostCard.php
+++ lib/View/Server/PostCard.php
@@ -8,7 +8,7 @@ class View_Server_PostCard extends \View{
 
 		$btn=$this->add('Button')->addClass('btn btn-primary')->set('manage post card');
 		
-		$btn->js('click')->univ()->frameURL('Create Post card' ,$this->api->url('xsocialApp_page_activitypages_createpostcard'));
+		$btn->js('click')->univ()->frameURL('Create Post card' ,$this->api->url('xsocialApp_page_activitypages_cpc'));
    	
 	}
 
