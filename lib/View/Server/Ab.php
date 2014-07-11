abe81365b5312d33334840d0a20e6897d48d499d
Rakesh <rakesh@rakesh-Vostro-1550.(none)>
2014-07-11 15:04:33 +0530

    working on add scrool fixed

--- lib/View/Server/Ab.php
+++ lib/View/Server/Ab.php
@@ -11,9 +11,9 @@ class View_Server_Ab extends \View{
 
 		$i=0;
 		foreach ($add_blocks as $junk) {
+			$lister = $this->add('xsocialApp/View_Lister_Ab');
 			$images = $this->add('xsocialApp/Model_BImg');
 			$images->addCondition('block_id',$add_blocks->id);
-			$lister = $this->add('xsocialApp/View_Lister_Ab');
 			$lister->setModel($images);
 			$lister->addClass('.carousel-'.$i);
 			$this->js(true)->carousel('.carousel-'.$i,$add_blocks['add_rotate_timing']*1000);
