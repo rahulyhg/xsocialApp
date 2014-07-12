d6aeb541d0cfe791190546aed6c2b7120fd19811
Khushbu Soni <khushbu@xavoc.com>
2014-07-12 19:54:36 +0530

    before push

---
+++ lib/View/Tools/LikeListTool.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace xsocialApp;
+
+class View_Tools_LikeListTool extends \editingToolbar\View_Tool {
+	public $title = 'xSocial Like List';
+	public $class='View_LikeList';
+	public $component_type='xSocialLikeList';
+	
+}
